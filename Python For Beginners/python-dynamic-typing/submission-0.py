4678ec02b94e64b31d734684bc9fa6ec7479f7f0
Miguel <miguel.trevinom@gmail.com>
2026-09-07 14:22:42 -0600

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 2;
+print(type(variable))
+variable = 2.2
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "M"
+print(type(variable))
+variable = [1,2]
+print(type(variable))
