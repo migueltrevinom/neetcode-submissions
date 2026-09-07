40f4a8cea3295f2740728d6d0f2ed5da5935f86a
Miguel <miguel.trevinom@gmail.com>
2026-09-06 22:46:16 -0600

    Add: baseball-game - submission-2

---
+++ Data Structures & Algorithms/baseball-game/submission-2.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {string[]} operations
+     * @return {number}
+     */
+    calPoints(operations) {
+        let result = 0;
+        const stack = [];
+
+        for (let index = 0; index < operations.length; index++) {
+            const operation = operations[index];
+
+            switch (operation) {
+                case '+':
+                    stack.push(Number(stack[stack.length-1] || 0) + Number(stack[stack.length-2] || 0)); 
+                    continue;
+                case 'D':
+                    stack.push(stack[stack.length -1] * 2);
+                    continue;
+
+                case 'C':
+                    stack.pop();
+                    continue;
+
+                default:
+                    stack.push(Number(operations[index]));
+                
+            }
+
+        }
+
+        for (let x = 0; x < stack.length; x++) {
+            result += Number(stack[x] ? stack[x] : 0);
+            
+        }
+
+        return result;
+    } 
+}
